1efa1c5cad69437f82a1abbe079341d8ac23a963
peihao.chen <chenpe@us.ibm.com>
2017-11-22 00:52:51 -0500

    posting user

--- application/__init__.py
+++ application/__init__.py
@@ -7,3 +7,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 from application import views
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+import SQLAlchemy_declear
+
+engine = create_engine('sqlite:///argo_food.db')
+try:
+    engine.connect()
+    engine.execute('SELECT 1;')
+except OperationalError:
+    SQLAlchemy_declear()
--- application/views.py
+++ application/views.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from SQLAlchemy_declear import User, Base
+from sqlalchemy.orm import sessionmaker
+
 from . import app
 
 @app.route('/')
@@ -9,3 +13,25 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if 'user' in data:
+        user_json = data['user']
+        name = None
+        email = None
+        if 'name' in user_json:
+            name = user_json['name']
+        if 'email' in user_json:
+            email = user_json['email']
+        engine = create_engine('sqlite:///argo_food.db')
+        Base.metadata.bind = engine
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+        user = User(name = name, email = email)
+        session.add(user)
+        session.commit()
+        return '',201
+    else:
+        return 'invalid data', 400
